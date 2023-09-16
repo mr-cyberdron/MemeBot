949871b2fb2f0738b5ff6b37ac7f2363195e1994
Sobkevych <denys@nxte.nl>
2023-09-16 18:31:29 +0300

    proc

--- DatabaseProcessor/FilesystemDatabaseProcessor.py
+++ DatabaseProcessor/FilesystemDatabaseProcessor.py
@@ -1,1 +1,19 @@
-#none
+from os import listdir
+from os.path import isfile, join
+import env
+class FileSystemDatabase:
+
+    def __init__(self):
+        pass
+
+    def generate_file(self):
+        audio_paths = listdir(env.audio_dir)
+        video_paths = listdir(env.video_dir)
+
+        return audio_paths
+
+
+
+a = FileSystemDatabase
+b = a.generate_file
+input(b)
