1c8302d9bc70c52414671684b0ad130787521595
Sobkevych <denys@nxte.nl>
2023-09-16 21:08:14 +0300

    filesystem processor

--- DatabaseProcessor/FilesystemDatabaseProcessor.py
+++ DatabaseProcessor/FilesystemDatabaseProcessor.py
@@ -1,19 +1,44 @@
-from os import listdir
-from os.path import isfile, join
+import os
+
+import pandas as pd
+import numpy as np
 import env
+
+# @todo add function generate_tags()
 class FileSystemDatabase:
 
-    def __init__(self):
+    def process_database(self):
+        meme_short_paths = self.get_db_memes_path_list()
+
+        tags_file_path = env.root + env.tags_file
+        tags_file = pd.read_csv(tags_file_path)
+        tag_paths = list(tags_file['path'].to_numpy())
+        # Get paths that not exist in db.
+        file_paths_to_append = set(meme_short_paths).difference(set(tag_paths))
+
+    '''
+    Get shorty meme paths from folder.
+    '''
+    def get_db_memes_path_list(self):
+        paths = []
+        # Absolute root memes folder path.
+        meme_dir = os.getcwd() + env.local_base_dir
+
+        # Absolute meme types folder paths.
+        dirs = [path for path in os.listdir(meme_dir) if os.path.isdir(meme_dir + path)]
+
+        for dir in dirs:
+            files = os.listdir(meme_dir + dir)
+            for file in files:
+                # Get meme db short path.
+                db_file_path = (meme_dir + file).split('/')
+                db_file_path = os.path.join('/', db_file_path[-3], db_file_path[-2], db_file_path[-1])
+                paths.append(db_file_path)
+
+        return paths
+
+    def get_unprocessed_paths(self):
+        # @todo move code here, write files to csv
+
         pass
 
-    def generate_file(self):
-        audio_paths = listdir(env.audio_dir)
-        video_paths = listdir(env.video_dir)
-
-        return audio_paths
-
-
-
-a = FileSystemDatabase()
-b = a.generate_file()
-input(b)
